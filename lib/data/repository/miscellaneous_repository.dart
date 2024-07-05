3bf4249b2a72a684c5cd7c8b26104c61a39910d7
Arjun Verma <kshatriya.arjun.developer@gmail.com>
2024-07-05 11:36:00 +0530

    REFACTOR: Code clean up

--- lib/data/repository/miscellaneous_repository.dart
+++ lib/data/repository/miscellaneous_repository.dart
@@ -1,4 +1,3 @@
-import 'package:boilerplate/data/data_source/local_data_source/user_local_data_source.dart';
 import 'package:boilerplate/data/data_source/remote_data_source/miscellaneous_remote_data_source.dart';
 import 'package:boilerplate/data/dto/response_dto/delete_dto/language_response_dto.dart';
 import 'package:boilerplate/utility/network/result.dart';
@@ -6,7 +5,6 @@ import 'package:boilerplate/utility/network/result.dart';
 class MiscellaneousRepository {
   final MiscellaneousRemoteDataSource remoteDataSource =
       MiscellaneousRemoteDataSource();
-  final UserLocalDataSource _localDataSource = UserLocalDataSource();
 
   // Making Singleton
   MiscellaneousRepository._();
